131097e2b9915253f2b0c6027bda7be4222e2e27
Jacob <jacobkeil13@gmail.com>
2022-06-03 15:30:32 -0400

    edits

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,31 +1,43 @@
-<h2>What is a gaming disorder?</h2>
+<center><h2>What is a gaming disorder?</h2></center>
+<br>
 <div class="flex">
   <p>According the World Health Organization (WHO), as a pattern of gaming behavior characterized by impaired control over gaming, increasing priority given to gaming over other activities to the extent that gaming takes precedence over other interests and daily activities, and continuation or escalation of gaming despite the occurrence of negative consequences.</p>
   <img src="https://i.guim.co.uk/img/media/a1248b9d1f8f61327ea68c974615105b59e20cad/0_79_4260_2556/master/4260.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=02be2ddc2728a36c4d36ec5741b9888b" alt="Guy playing a game">
 </div>
 <br>
-<h2>How can this be problematic?</h2>
-<p></p>
+<center><h2>How can this be problematic?</h2></center>
+<br>
+<div class="flex-reverse">
+  <img src="https://i.guim.co.uk/img/media/a1248b9d1f8f61327ea68c974615105b59e20cad/0_79_4260_2556/master/4260.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=02be2ddc2728a36c4d36ec5741b9888b" alt="Guy playing a game">
+  <p>According the World Health Organization (WHO), as a pattern of gaming behavior characterized by impaired control over gaming, increasing priority given to gaming over other activities to the extent that gaming takes precedence over other interests and daily activities, and continuation or escalation of gaming despite the occurrence of negative consequences.</p>
+</div>
 
 <style>
   img {
     width: 100%;
     border-radius: 1rem;
   }
+
   .flex {
     display: grid;
     gap: 1rem;
     grid-template-columns: 2fr 1fr;
   }
 
+  .flex-reverse {
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: 1fr 2fr;
+  }
+
   @media only screen and (max-width: 800px) {
-    .flex {
+    .flex, .flex-reverse {
       grid-template-columns: 1fr 1fr;
     }
   }
 
   @media only screen and (max-width: 750px) {
-    .flex {
+    .flex, .flex-reverse {
       grid-template-columns: 1fr;
     }
   }
